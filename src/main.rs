1d72c3d1e75189f0755095e986ac8045322ce31f
Georgi Martsenkov <g.martsenkov@gmail.com>
2021-09-14 23:59:45 +0100

    setup basic template and input data

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use handlebars::Handlebars;
-use serde_json::json;
+use serde_json::Value;
 use std::error::Error;
+use std::fs;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let reg = Handlebars::new();
+    let template = fs::read_to_string("design/template.html.mustache").expect("Could not load template");
+
     // render without register
-    println!(
-        "{}",
-        reg.render_template("Hello {{name}}", &json!({"name": "foo"}))?
-    );
+    let data = fs::read_to_string("data.json")?;
+    let data_json : &Value = &serde_json::from_str(&data)?;
+
+    fs::write("output.html", reg.render_template(template.as_str(), data_json)?)?;
+
     Ok(())
 }
