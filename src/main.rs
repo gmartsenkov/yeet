c263c278f12ede1da738e38c7ea50e7733605f8f
Georgi Martsenkov <g.martsenkov@gmail.com>
2021-09-14 23:35:18 +0100

    add handlebars and serde_json

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use handlebars::Handlebars;
+use serde_json::json;
+use std::error::Error;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let reg = Handlebars::new();
+    // render without register
+    println!(
+        "{}",
+        reg.render_template("Hello {{name}}", &json!({"name": "foo"}))?
+    );
+    Ok(())
 }
