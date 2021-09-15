f27517a9304347ff0d7c8964c98bafb3741962d3
Georgi Martsenkov <g.martsenkov@gmail.com>
2021-09-15 09:00:53 +0100

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,17 @@ use std::fs;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let reg = Handlebars::new();
-    let template = fs::read_to_string("design/template.html.mustache").expect("Could not load template");
+    let template =
+        fs::read_to_string("design/template.html.mustache").expect("Could not load template");
 
     // render without register
     let data = fs::read_to_string("data.json")?;
-    let data_json : &Value = &serde_json::from_str(&data)?;
+    let data_json: &Value = &serde_json::from_str(&data)?;
 
-    fs::write("output.html", reg.render_template(template.as_str(), data_json)?)?;
+    fs::write(
+        "output.html",
+        reg.render_template(template.as_str(), data_json)?,
+    )?;
 
     Ok(())
 }
