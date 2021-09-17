972c29246be1f00f3f2d7199243fcaef1c69e08e
Georgi Martsenkov <g.martsenkov@gmail.com>
2021-09-17 17:25:22 +0100

    add education section

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,20 @@ fn main() -> Result<(), Box<dyn Error>> {
     let mut reg = Handlebars::new();
     let template = include_str!("design/template.html.hbs");
 
-    reg.register_template_string("experiences", include_str!("design/partials/experience.hbs"))?;
+    reg.register_template_string(
+        "experiences",
+        include_str!("design/partials/experience.hbs"),
+    )?;
+    reg.register_template_string(
+        "education",
+        include_str!("design/partials/education.hbs"),
+    )?;
 
     // render without register
     let data = fs::read_to_string("data.json")?;
     let data_json: &Value = &serde_json::from_str(&data)?;
 
-    fs::write(
-        "output.html",
-        reg.render_template(template, data_json)?,
-    )?;
+    fs::write("output.html", reg.render_template(template, data_json)?)?;
 
     Ok(())
 }
